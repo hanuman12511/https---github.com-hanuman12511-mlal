159893231879bd5fc4a87648d7cdd5263b29fc95
hanuman12511 <er.hanumansaini@gmail.com>
2025-05-23 09:28:16 +0530

    readcsv

--- Pandas1.py
+++ Pandas1.py
@@ -1,15 +1,54 @@
 import pandas as pd
 import numpy as np
 
-# index= ["r1","r2","r3"]
-df =pd.DataFrame(np.array([np.random.rand(6),np.random.rand(6),np.random.rand(6)]) )
+
+id=[1,2,3,4,5]
+name=["user1","user2","user3","user4","user5"]
+age = [22,33,44,55,66]
+
+
+df = pd.DataFrame([id,name,age],index=["id","name","age"],columns=["c1","c2","c3","c4","c5"])
+
 
 print(df)
+# print(df.loc["id"].values.reshape((-1,1)))
+# print(df.loc[["id","name"]])
+# print(df.loc[:,"c2"])
+print(df.loc["name","c2"])
+print(df.loc["age","c2"])
+print(df.loc[["name","age"],["c2","c4"]])
+print(df.loc[["name","age"],"c2":])
+print(df.loc[["name","age"],:"c4"])
+
+# print(df)
+# df["c5"] = [6,"user5",23]
+# print(df)
+# print(df['c2'])
+# print(df["id"::2])
+# df[:"salary"] = [22,55,55,5,5]
+
+# df.update({"age":[30,303,303,404,40]})
+# pd.concat(df,pd.DataFrame([{"salary":[30,303,303,404,40]}]))
+# print(df)
+
+# x= np.linspace(-4,4,9)
+# y= np.linspace(-5,5,11)
+
+# x1,y1 = np.meshgrid(x,y)
+
+# print(x1)
+# print(y1)
+
+
+# index= ["r1","r2","r3"]
+# df =pd.DataFrame(np.array([np.random.rand(6),np.random.rand(6),np.random.rand(6)]) )
+
+# print(df)
 # print(df[2])
 # print(df[2][2])
 # print(df[1:])
 # print(df[1:,2:])
-print(df.loc[1:2,1])
+# print(df.loc[1:2,1])
 # dict1= {"id":[1,2,3,4],"name":["a","b","c","d"],"salary":[2,3,4,5]}
 
 # df = pd.DataFrame(data=dict1)
---
+++ ReadCSV.py
@@ -0,0 +1,5 @@
+import pandas as pd
+df = pd.read_csv('test.csv')
+# print(df)
+# print(df.head())
+print(df.head(20))
